17c027c2ecc1e0634533e53b625b0bc43a170286
unknown <marianabarros.fb@gmail.com>
2024-08-29 16:40:36 -0400

    Criado construtor da parede e sua randerização

--- src/heras_game/Parede.java
+++ src/heras_game/Parede.java
@@ -1,5 +1,21 @@
 package heras_game;
 
+import java.awt.Graphics;
+import java.awt.Color;
+
 public class Parede {
+    int x, y, largura, altura;
+
+    public Parede(int x, int y, int altura, int largura) {
+        this.x = x;
+        this.y = y;
+        this.altura = altura;
+        this.largura = largura;
+    }
+
+    public void randerizar(Graphics g) {
+        g.setColor(Color.RED);
+        g.fillRect(x, y, largura, altura);
+    }
 
 }
